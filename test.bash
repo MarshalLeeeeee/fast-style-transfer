f86eb5656db6e7c5649c86d14c44c0bafe90c5e2
marshallee <marshallee413lmc@sina.com>
2018-04-30 13:32:18 +0800

    bash file for testing

---
+++ test.bash
@@ -0,0 +1,4 @@
+python style.py --checkpoint-dir checkpoint --fcrs-weight=0 --fcon-weight=0 --fdir-weight=1e3
+python style.py --checkpoint-dir checkpoint --fcrs-weight=0 --fcon-weight=0 --fdir-weight=1e4
+python style.py --checkpoint-dir checkpoint --fcrs-weight=0 --fcon-weight=0 --fdir-weight=1e5
+python style.py --checkpoint-dir checkpoint --fcrs-weight=0 --fcon-weight=0 --fdir-weight=1e6
